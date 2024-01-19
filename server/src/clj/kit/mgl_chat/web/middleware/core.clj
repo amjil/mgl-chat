37e3deebeb8324a51930cf51dc2bdbc71b74da88
amjil <amjil@protonmail.com>
2024-01-19 23:56:58 +0800

    wrap restricted

--- server/src/clj/kit/mgl_chat/web/middleware/core.clj
+++ server/src/clj/kit/mgl_chat/web/middleware/core.clj
@@ -1,14 +1,29 @@
 (ns kit.mgl-chat.web.middleware.core
   (:require
-    [kit.mgl-chat.env :as env]
-    [ring.middleware.defaults :as defaults]
-    [ring.middleware.session.cookie :as cookie]))
+   [kit.mgl-chat.env :as env]
+   
+   [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]
+   [buddy.auth.backends.token :refer [jwe-backend]]
+   [buddy.auth.accessrules :refer [restrict]]
+   [buddy.auth :refer [authenticated?]]
+   [buddy.core.hash :as hash]
+
+   [ring.middleware.defaults :as defaults]
+   [ring.middleware.session.cookie :as cookie]))
+
+(defn on-error [request response]
+  {:status 403
+   :headers {}
+   :body (str "Access to " (:uri request) " is not authorized")})
+
+(defn wrap-restricted [handler]
+  (restrict handler {:handler authenticated?
+                     :on-error on-error}))
 
 (defn wrap-base
   [{:keys [metrics site-defaults-config cookie-secret] :as opts}]
   (let [cookie-store (cookie/cookie-store {:key (.getBytes ^String cookie-secret)})]
     (fn [handler]
       (cond-> ((:middleware env/defaults) handler opts)
-              true (defaults/wrap-defaults
-                     (assoc-in site-defaults-config [:session :store] cookie-store))
-              ))))
+        true (defaults/wrap-defaults
+              (assoc-in site-defaults-config [:session :store] cookie-store))))))
