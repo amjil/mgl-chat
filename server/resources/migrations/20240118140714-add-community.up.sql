97f6c3089f9cace8effad816a7b29d9f3b754042
amjil <amjil@protonmail.com>
2024-01-22 22:48:29 +0800

    communityn create update delete

--- server/resources/migrations/20240118140714-add-community.up.sql
+++ server/resources/migrations/20240118140714-add-community.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS communities (
     id uuid DEFAULT uuid_generate_v4 () primary key,
     title varchar(100),
+    created_by uuid NOT NULL REFERENCES users ("id") ON DELETE CASCADE,
     "created_at" timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
--- server/src/clj/kit/mgl_chat/web/controllers/community.clj
+++ server/src/clj/kit/mgl_chat/web/controllers/community.clj
@@ -1,1 +1,30 @@
-(ns kit.mgl-chat.web.controllers.community)
+(ns kit.mgl-chat.web.controllers.community
+  (:require
+   [kit.mgl-chat.web.utils.db :as db])
+  (:import
+   [java.util UUID]))
+
+(defn new-community
+  [conn uinfo params]
+  (db/insert!
+   conn
+   :communities
+   (assoc params :user_id (UUID/fromString (:id uinfo))))
+  {})
+
+(defn delete-community
+  [conn uinfo id]
+  (db/delete!
+   conn
+   :communities
+   {:id (UUID/fromString id)
+    :user_id (UUID/fromString (:id uinfo))}))
+
+(defn update-community
+  [conn uinfo id params]
+  (db/update!
+   conn
+   :communities
+   params
+   {:id (UUID/fromString id)
+    :user_id (UUID/fromString (:id uinfo))}))
